27d100e28bce1a472a24a0f8b9cff4e49bc973b9
Jan Krukowski <jan.krukowski@geneva.com>
2024-01-04 15:17:16 +0100

    fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,10 @@ let checksum = "958970064322013ff4fa92ecb0ba53cea4eb3fce41be29498eb645e531f4fe6d
 let package = Package(
     name: "OpenMP",
     platforms: [
-        .macOS(.v13_3), .iOS(.v13), .watchOS(.v6_0), .tvOS(.v6_0)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         .library(
@@ -18,7 +21,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenMP",
-            url: "https://github.com/eugenehp/openmp-mobile/releases/download/v\(version)/openmp.xcframework.zip"
+            url: "https://github.com/DeveloperMindset-com/openmp-mobile/releases/download/v\(version)/openmp.xcframework.zip",
             checksum: checksum
         )
     ]
