70ae77ddda76418bdf395f43ead4f01a33d9247d
Alex C. Schaefer <alexcschaefer@me.com>
2024-01-08 21:00:49 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,10 +7,7 @@ let checksum = "958970064322013ff4fa92ecb0ba53cea4eb3fce41be29498eb645e531f4fe6d
 let package = Package(
     name: "OpenMP",
     platforms: [
-        .macOS(.v13_3),
-        .iOS(.v13),
-        .tvOS(.v6_0),
-        .watchOS(.v6_0)
+        .macOS(.v13_3), .iOS(.v13), .watchOS(.v6_0), .tvOS(.v6_0)
     ],
     products: [
         .library(
@@ -21,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenMP",
-            url: "https://github.com/eugenehp/openmp-mobile/releases/download/v\(version)/openmp.xcframework.zip",
+            url: "https://github.com/DeveloperMindset-com/openmp-mobile/releases/download/v\(version)/openmp.xcframework.zip",
             checksum: checksum
         )
     ]
